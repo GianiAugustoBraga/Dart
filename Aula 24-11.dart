44d55b2b6be13424b7799779acd50f0977a2e6a3
GianiAugustoBraga <115471128+GianiAugustoBraga@users.noreply.github.com>
2022-11-24 09:20:59 -0300

    Add files via upload

---
+++ Aula 24-11.dart
@@ -0,0 +1,37 @@
+void main() {
+    
+  // Exibindo olá mundo na tela
+  
+  //print('Olá, Mundo!');
+  
+  // Criação de variáveis
+  int idade = 29;
+  String nome = "Giani";
+  double salario = 1.200;
+  
+  // Exibindo as informações das variáveis
+  //print(nome);
+ //print(idade);
+  //print(salario);
+  
+  double n1 = 5.2;
+  double n2 = 7.8;
+  double n3 = 2;
+  double total = n1 - n2;
+  double resultado = (n1 * n2) / n3;
+  
+  print(total);
+  print(resultado);
+  
+  n1 *= 10;
+  print(n1);
+  
+  // Exercício 1: calcular a média das notas de um aluno
+  double nota1 = 10;
+  double nota2 = 10;
+  double media = (nota1 + nota2)/2;
+  print(media);
+  
+  
+  
+}
